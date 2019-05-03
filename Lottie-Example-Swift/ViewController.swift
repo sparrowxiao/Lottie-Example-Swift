eecb2a2151ad1399a3612bd185d6983f8a476d3b
Ting Shoemaker <tshoemaker@easycare.com>
2019-05-03 12:02:42 -0400

    Initial Commit

---
+++ Lottie-Example-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lottie-Example-Swift
+//
+//  Created by Ting_SH on 5/3/19.
+//  Copyright © 2019 Ting_SH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
